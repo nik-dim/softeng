1a9b24c81b49cf7f758a00cea5f28a9a0ce85d50
xspirus <spirosdontas@gmail.com>
2018-12-22 16:25:45 +0200

    Node modules. MapBoxComponent. MapService. GeoJSON file

---
+++ frontend/src/app/map-box/map-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-box',
+  templateUrl: './map-box.component.html',
+  styleUrls: ['./map-box.component.scss']
+})
+export class MapBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
