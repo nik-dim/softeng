83c9a7dd04adbf0452eb29ffc7b6510ffea2cb89
xspirus <spirosdontas@gmail.com>
2019-02-27 18:52:09 +0200

    Minor

--- backend_node/api/routes/products.js
+++ backend_node/api/routes/products.js
@@ -7,7 +7,7 @@ const ProductsController = require('../controllers/products');
 
 router.get('/', ProductsController.products_get_all);
 
-router.post('/', checkAuth, authorize('User'),  ProductsController.products_create_product);
+router.post('/', checkAuth, authorize('User'), ProductsController.products_create_product);
 
 router.get('/:id', ProductsController.products_get_product);
 
