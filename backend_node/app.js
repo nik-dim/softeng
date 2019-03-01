d3a7a27aeb8738ee993bf52f1b91b8350a580d8a
xspirus <spirosdontas@gmail.com>
2019-03-01 19:36:13 +0200

    Fixed x-observatory-auth in server side CORS policy

--- backend_node/app.js
+++ backend_node/app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // *: give acces to all origins
     res.header(
         'Access-Control-Allow-Headers',
-        'Origin, X-Request-With, Content-Type, Accept, Authorization'
+        'Origin, X-Request-With, Content-Type, Accept, Authorization, X-OBSERVATORY-AUTH'
     );
 
     if (req.method === 'OPTIONS') {
