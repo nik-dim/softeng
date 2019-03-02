e109ba0d4819ab58abb84d6637a32e6f5ff04d00
xspirus <spirosdontas@gmail.com>
2019-03-02 21:12:11 +0200

    New pages

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+
 import { MapComponent } from '@components/map/map.component';
 import { NotFoundComponent } from '@components/not-found/not-found.component';
+import { LoginComponent } from '@components/user/login/login.component';
+import { SignupComponent } from '@components/user/signup/signup.component';
+import { ProductsComponent } from '@components/products/products/products.component';
 
 const routes: Routes = [
   { path: "", component: MapComponent },
+  { path: "login", component: LoginComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "products", component: ProductsComponent },
   { path: "**", component: NotFoundComponent },
 ];
 
