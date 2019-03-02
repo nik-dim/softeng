c01f0f59f1f32d73f1964a38a99ce46e2f18a1df
xspirus <spirosdontas@gmail.com>
2019-03-02 21:12:43 +0200

    Updated nav to check when user is logged in

--- web/src/app/components/nav/nav.component.ts
+++ web/src/app/components/nav/nav.component.ts
@@ -1,20 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { AuthenticationService } from '@services/authentication.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent{
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private auth: AuthenticationService,
+  ) {  }
+
+  logout() {
+    this.auth.logout().subscribe(a => location.reload(true))
+  }
 
 }
