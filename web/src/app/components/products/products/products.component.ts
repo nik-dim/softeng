a405453675ad1f9666183d6a4179d76ebe9417d1
xspirus <spirosdontas@gmail.com>
2019-03-02 21:13:28 +0200

    Added product implementation

--- web/src/app/components/products/products/products.component.ts
+++ web/src/app/components/products/products/products.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from '@services/product.service';
+import { Product } from '@models/product';
+import { MatSort, MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  products: Product[];
+  dataSource;
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  displayedColumns = [
+    'name',
+    'category',
+    'description',
+    '_id',
+  ]
+
+  constructor(
+    private router: Router,
+    private productService: ProductService,
+  ) { }
 
   ngOnInit() {
+    this.initProducts();
+  }
+
+  private initProducts() {
+    this.productService.getProducts()
+    .subscribe(products => {
+      this.products = products.map(product => {
+        return new Product(product);
+      });
+      this.dataSource = new MatTableDataSource(this.products);
+      this.dataSource.sort = this.sort;
+    });
   }
 
 }
--- web/src/app/models/product.ts
+++ web/src/app/models/product.ts
@@ -1,2 +1,26 @@
 export class Product {
+  _id?: number;
+  name: string;
+  description: string;
+  category: string;
+  tags: string[];
+  withdrawn?: boolean;
+
+  constructor(options?: {
+    _id?: number,
+    name: string,
+    description: string,
+    category: string,
+    tags: string[],
+    withdrawn?: boolean,
+  }) {
+    if ("_id" in options)
+      this._id = options._id;
+    this.name = options.name;
+    this.description = options.description;
+    this.category = options.category;
+    this.tags = options.tags;
+    if ("withdrawn" in options)
+      this.withdrawn = options.withdrawn;
+  }
 }
