b91c2e09467143d9ce08a85653a671c84404af9e
xspirus <spirosdontas@gmail.com>
2019-03-03 23:11:17 +0200

    Unauthorized page

---
+++ web/src/app/components/unauthorized/unauthorized.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unauthorized',
+  templateUrl: './unauthorized.component.html',
+  styleUrls: ['./unauthorized.component.scss']
+})
+export class UnauthorizedComponent implements OnInit {
+
+  loading = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
