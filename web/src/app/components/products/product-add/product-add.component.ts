769bc2a972624d02c690b185e6ab10d3dd4211d6
xspirus <spirosdontas@gmail.com>
2019-03-03 23:10:42 +0200

    Implemented product addition

--- web/src/app/components/products/product-add/product-add.component.ts
+++ web/src/app/components/products/product-add/product-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProductService } from '@services/product.service';
+import { Product } from '@models/product';
 
 @Component({
   selector: 'app-product-add',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductAddComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  categories = [
+    'Accessories',
+    'Boiler Fuel',
+    'Car Essentials',
+    'Car Fuel',
+    'Cleaning',
+    'Home Usage',
+  ]
+  tags: string;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private productService: ProductService,
+  ) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      name: [null, Validators.required],
+      category: [null, Validators.required],
+      description: [null, Validators.required],
+      tags: [null, Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid)
+      return;
+    this.tags = this.form.value.tags;
+    let atags = this.tags.split(",").map(a => a.trim());
+    this.form.value.tags = atags;
+    let value = this.form.value;
+    this.productService.addProduct(new Product(value))
+    .subscribe(a => this.router.navigate(['/products']));
   }
 
 }
