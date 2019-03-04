da2fa4e34bb3dc72d610515338f6f90d3033bd65
xspirus <spirosdontas@gmail.com>
2019-03-04 14:01:49 +0200

    Finished map component
    Co-authored-by: thanasislerai <thanasisleraj@yahoo.com>

--- web/src/app/components/map/map.component.ts
+++ web/src/app/components/map/map.component.ts
@@ -6,6 +6,7 @@ import * as turf from '@turf/turf';
 import { ShopService } from '@services/shop.service';
 import { Shop } from '@models/shop';
 import { environment } from '../../../environments/environment';
+import { AuthenticationService } from '@services/authentication.service';
 
 @Component({
   selector: 'app-map',
@@ -28,7 +29,10 @@ export class MapComponent implements OnInit {
   geojson: FeatureCollection;
   shoplist: any[];
 
-  constructor(private shopService: ShopService) { 
+  constructor(
+    private shopService: ShopService,
+    private auth: AuthenticationService,
+  ) { 
     mapboxgl.accessToken = environment.mapbox.accessToken;
   }
 
@@ -52,7 +56,6 @@ export class MapComponent implements OnInit {
         return val.toGeoJSON();
       })
     );
-    console.log(this.geojson);
 
     this.initialiseMap();
   }
@@ -76,6 +79,7 @@ export class MapComponent implements OnInit {
     });
 
     this.map.on('load', (event) => {
+      this.sidenav.close();
       this.map.addLayer({
         id: 'locations',
         type: 'circle',
@@ -84,8 +88,8 @@ export class MapComponent implements OnInit {
           data: this.geojson
         },
         paint: {
-          'circle-radius': this.map.getZoom() - 2,
-          'circle-color': '#007cbf',
+          'circle-radius': 8,
+          'circle-color': '#0c4e04',
           'circle-stroke-width': 1,
           'circle-stroke-color': '#fff'
         },
@@ -107,6 +111,8 @@ export class MapComponent implements OnInit {
       this.map.addControl(locate);
 
       locate.on('geolocate', (event) => {
+        console.log('geolocate');
+        this.sidenav.open();
         this.lng = event.coords.longitude;
         this.lat = event.coords.latitude;
 
@@ -124,7 +130,6 @@ export class MapComponent implements OnInit {
           });
         });
         var myJson = JSON.stringify(this.geojson);
-        console.log(myJson);
 
         this.geojson.features.sort(function(a, b) {
           if (a.properties.distance > b.properties.distance) {
@@ -137,10 +142,10 @@ export class MapComponent implements OnInit {
           return 0;
         });
 
-        var listings = document.getElementById('listings');
-        while (listings.firstChild) {
-          listings.removeChild(listings.firstChild);
-        }
+        //var listings = document.getElementById('listings');
+        //while (listings.firstChild) {
+        //  listings.removeChild(listings.firstChild);
+        //}
 
         this.buildLocationList(this.geojson);
       });
@@ -178,17 +183,12 @@ export class MapComponent implements OnInit {
       // Query all the rendered points in the view
       var features = this.map.queryRenderedFeatures(event.point, { layers: ['locations'] });
       if (features.length) {
-        this.sidenav.toggle();
         var clickedPoint = features[0];
         // 1. Fly to the point
         this.flyToStore(clickedPoint);
         // 2. Close all other popups and display popup for clicked store
         this.createPopUp(clickedPoint);
         // 3. Highlight listing in sidebar (and remove highlight for all other listings)
-        var activeItem = document.getElementsByClassName('active');
-        if (activeItem[0]) {
-          activeItem[0].classList.remove('active');
-        }
         // Find the index of the store.features that corresponds to the clickedPoint that fired the event listener
         //var selectedFeature1 = clickedPoint.properties.name;
         var selectedFeature = clickedPoint.properties._id;
@@ -198,12 +198,6 @@ export class MapComponent implements OnInit {
             var selectedFeatureIndex = i;
           }
         }
-        // Select the correct list item using the found index and add the active class
-        var listing = document.getElementById('listing-' + selectedFeatureIndex);
-        console.log(listing)
-      }
-      else {
-        this.sidenav.toggle();
       }
     });
 
@@ -230,7 +224,7 @@ export class MapComponent implements OnInit {
         type: 'circle',
         paint: {
           'circle-radius': 10,
-          'circle-color': '#007cbf',
+          'circle-color': '#af1e19',
           'circle-stroke-width': 3,
           'circle-stroke-color': '#fff'
         }
@@ -240,6 +234,7 @@ export class MapComponent implements OnInit {
       // `result` event is triggered when a user makes a selection
       // Add a marker at the result's coordinates
       geocoder.on('result', (event) => {
+        this.sidenav.open();
         var searchResult = event.result.geometry;
         this.map.getSource('single-point').setData(searchResult);
         console.log(searchResult);
@@ -267,72 +262,40 @@ export class MapComponent implements OnInit {
           return 0;
         });
 
-        var listings = document.getElementById('listings');
-        while (listings.firstChild) {
-          listings.removeChild(listings.firstChild);
-        }
+        //var listings = document.getElementById('listings');
+        //while (listings.firstChild) {
+        //  listings.removeChild(listings.firstChild);
+        //}
 
         this.buildLocationList(this.geojson);
+        
       });
     });
 
   }
 
   private buildLocationList(data) {
-    // Iterate through the list of stores
-    for (var i = 0; i < 10; i++) {
-      var currentFeature = data.features[i];
-      // Shorten data.feature.properties to just `prop` so we're not
-      // writing this long form over and over again.
-      var prop = currentFeature.properties;
-      // Select the listing container in the HTML and append a div
-      // with the class 'item' for each store
-      var listings = document.getElementById('listings');
-      var listing = listings.appendChild(document.createElement('div'));
-      listing.className = 'item';
-      listing.id = 'listing-' + i;
 
-      // Create a new link with the class 'title' for each store
-      // and fill it with the store address
-      let link = listing.appendChild(document.createElement('a'));
-      link.className = 'title';
-      (link as any).dataPosition = i;
-      link.innerHTML = prop.name;
-
-      // Event listener
-      // Add an event listener for the links in the sidebar listing
-     link.addEventListener('click', (event) => {
-        // Update the currentFeature to the store associated with the clicked link
-        var clickedListing = data.features[(link as any).dataPosition];
-        console.log(clickedListing);
-        // 1. Fly to the point associated with the clicked link
-        this.flyToStore(clickedListing);
-        // 2. Close all other popups and display popup for clicked store
-        this.createPopUp(clickedListing);
-        // 3. Highlight listing in sidebar (and remove highlight for all other listings)
-        var activeItem = document.getElementsByClassName('active');
-        if (activeItem[0]) {
-          activeItem[0].classList.remove('active');
-        }
-        listing.classList.add('active');
-      });
-
-      //----------------------------------------------------------------------------------------------------
-
-      if (prop.distance) {
-        var roundedDistance = Math.round(prop.distance * 100) / 100;
-        var details = listing.appendChild(document.createElement('div'));
-        details.innerHTML += '<p><strong>' + roundedDistance + ' km away</strong></p>';
-      }
-
-      // Create a new div with the class 'details' for each store
-      // and fill it with the city and phone number
-      //var details = listing.appendChild(document.createElement('div'));
-      //details.innerHTML = prop.city;
-      //if (prop.phone) {
-      //  details.innerHTML += ' &middot; ' + prop.phoneFormatted;
-      //}
+    this.shoplist = []
+    for (let i = 0; i < 10; i++) {
+      let current = data.features[i];
+      current.properties.distance = Math.round(
+        current.properties.distance * 100
+      ) / 100;
+      this.shoplist.push(current);
     }
+    console.log(this.shoplist);
+
+    // if (prop.distance) {
+      // var roundedDistance = Math.round(prop.distance * 100) / 100;
+      // var details = listing.appendChild(document.createElement('div'));
+      // details.innerHTML += '<p><strong>' + roundedDistance + ' km away</strong></p>';
+    // }
+  }
+
+  clickShop(data) {
+    this.flyToStore(data);
+    this.createPopUp(data);
   }
 
   private flyToStore(currentFeature) {
@@ -344,14 +307,20 @@ export class MapComponent implements OnInit {
 
   private createPopUp(currentFeature) {
     var popUps = document.getElementsByClassName('mapboxgl-popup');
-    console.log(popUps);
     // Check if there is already a popup on the map and if so, remove it
     if (popUps[0]) popUps[0].remove();
 
-    var popup = new mapboxgl.Popup({ closeOnClick: false })
-      .setLngLat(currentFeature.geometry.coordinates)
-      .setHTML('<h3>' + currentFeature.properties.name + '</h3>')
-      .addTo(this.map);
+    if (this.auth.isLoggedIn()) {
+      var popup = new mapboxgl.Popup({ closeOnClick: false })
+        .setLngLat(currentFeature.geometry.coordinates)
+        .setHTML('<h3><b><a href="shops/' + currentFeature.properties._id + '" target="_self">' + currentFeature.properties.name + '</a></b></h3></br><p><a href="prices/' + currentFeature.properties._id + '" target="_self">Add price</a></p>')
+        .addTo(this.map);
+    } else {
+      var popup = new mapboxgl.Popup({ closeOnClick: false })
+        .setLngLat(currentFeature.geometry.coordinates)
+        .setHTML('<h3><b><a href="shops/' + currentFeature.properties._id + '" target="_self">' + currentFeature.properties.name + '</a></b></h3>')
+        .addTo(this.map);
+    }
   }
 
 }
